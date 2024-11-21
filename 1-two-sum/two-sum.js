b860f969e541f7b3230f75e5ec412ef8c902c38e
ji_hyeok_ju <109324478+seotai@users.noreply.github.com>
2024-11-22 00:24:56 +0900

    Time: 1 ms (89.22%) | Memory: 52.2 MB (7.58%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,10 +4,14 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    let lens = nums.length
+    let mapp = new Map(), lens = nums.length
     for (let i = 0; i < lens; i++) {
-        for (let j = i + 1; j < lens; j++) {
-            if (nums[i] + nums[j] === target) return [i, j]
+        const pair = target - nums[i]
+        if (mapp.has(pair)) {
+            return [mapp.get(pair), i]
+        } else {
+            mapp.set(nums[i], i)
         }
     }
+
 };
