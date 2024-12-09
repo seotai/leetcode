de4bb82e0b11048b9275cc5639335bdc6ec369f1
ji_hyeok_ju <109324478+seotai@users.noreply.github.com>
2024-12-09 20:39:02 +0900

    Time: 6 ms (63.47%) | Memory: 52.9 MB (97.02%) - LeetSync

---
+++ 130-surrounded-regions/surrounded-regions.js
@@ -0,0 +1,31 @@
+/**
+ * @param {character[][]} board
+ * @return {void} Do not return anything, modify board in-place instead.
+ */
+var solve = function(board) {
+    const r = board.length, c = board[0].length
+    const dfs = (x, y) => {
+        if (x<0 || y<0 || x>=r || y>=c || board[x][y]==='visited' || board[x][y]==='X') return
+        board[x][y] = 'visited'
+        dfs(x+1, y)
+        dfs(x, y+1)
+        dfs(x-1, y)
+        dfs(x, y-1)
+        return
+    }
+    for (let i = 0; i < r; i ++) {
+        for (let j = 0; j < c; j ++) {
+            if ((i===0 || j===0 || i===r-1 || j===c-1) && board[i][j]==='O' ) dfs(i, j)
+        }
+    }
+    for(let i = 0; i < board.length; i++) {
+        for(let j = 0; j < board[0].length; j++) {
+            if(board[i][j] === 'visited') {
+                board[i][j] = 'O';
+            } else  {
+                board[i][j] = 'X';
+            }
+        }    
+    }
+    
+};
