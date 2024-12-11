181afcfafdd7118b42291341e0cf69c0184e0f97
ji_hyeok_ju <109324478+seotai@users.noreply.github.com>
2024-12-11 12:51:28 +0900

    Time: 0 ms (100.00%) | Memory: 49 MB (57.66%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.js
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.js
@@ -3,24 +3,25 @@
  * @return {string[]}
  */
 var letterCombinations = function(digits) {
-    const words = { 
-        2 : 'abc',
-        3 : 'def', 
-        4 : 'ghi', 
-        5 : 'jkl', 
-        6 : 'mno', 
-        7 : 'pqrs', 
-        8 : 'tuv', 
-        9 : 'wxyz' 
+    const mapp = {
+        '2' : 'abc',
+        '3' : 'def',
+        '4' : 'ghi',
+        '5' : 'jkl',
+        '6' : 'mno',
+        '7' : 'pqrs',
+        '8' : 'tuv',
+        '9' : 'wxyz'
     }
-    let ans = []
-    if (!digits.length) return []
-    function permute(pos, str) {
-        if (pos === digits.length) ans.push(str)
-        else {
-            let letters = words[digits[pos]]
-            for (let i = 0; i < letters.length; i++) {
-                permute(pos + 1, str + letters[i])
+    let leng = digits.length, ans = []
+    if (leng === 0) return []
+    const permute = (pos, str) => {
+        if (pos === leng) {
+            ans.push(str)
+        } else {
+            let letters = mapp[digits[pos]]
+            for (let letter of letters) {
+                permute(pos + 1, str + letter)
             }
         }
     }
